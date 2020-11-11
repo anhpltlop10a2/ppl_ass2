d88b31ead5a1942f29036859c46bd404235cebcb
Your Name <anh.phan1811442@hcmut.edu.vn>
2020-11-11 20:27:11 +0700

    intial_code2

--- initial/src/main/bkit/astgen/ASTGeneration.py
+++ initial/src/main/bkit/astgen/ASTGeneration.py
@@ -4,7 +4,7 @@ from AST import *
 
 class ASTGeneration(BKITVisitor):
     def visitProgram(self,ctx:BKITParser.ProgramContext):
-        return Program([VarDecl(Id(ctx.ID().getText()),[],None)])
+        return None
 
     
 
